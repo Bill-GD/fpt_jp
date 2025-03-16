4fb987b658bdddc06b5c84eb2f0663598407b61a
Bill-GD <geometrydashbill@gmail.com>
2025-03-16 16:01:18 +0700

    batman
    setup project

---
+++ README.md
@@ -0,0 +1,3 @@
+# fpt_jp
+
+An Android app to keep track of the Japanese at FPT course. Used for test/exam revision.
---
+++ android/app/src/main/kotlin/com/billgd/fpt_jp/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.billgd.fpt_jp
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
